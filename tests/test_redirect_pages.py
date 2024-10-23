0c1af312903f9c62259d2a03f638d3fcb818372b
Ivan <144273733+IvanTcanga@users.noreply.github.com>
2024-10-23 15:16:23 +0300

    Update test_redirect_pages.py
    Исправлены замечания по поводу локаторов в тестах.

--- tests/test_redirect_pages.py
+++ tests/test_redirect_pages.py
@@ -1,24 +1,22 @@
 import allure
 from pages.main_page import MainPage
-from locators.main_page_locators import MainPageLocators
-from data import DataForTests
 
 
 class TestLogoRedirect:
     @allure.title('Проверка, при клике на логотип «Самоката», попадёшь на главную страницу «Самоката».')
     def test_logo_redirect_to_main_page(self, driver):
         main_page = MainPage(driver)
-        main_page.find_element_with_wait(MainPageLocators.LOGO_SCOOTER)
-        main_page.click_to_element(MainPageLocators.ORDER_BUTTON_HEADER)
-        main_page.find_element_with_wait(MainPageLocators.LOGO_SCOOTER)
-        main_page.click_to_element(MainPageLocators.LOGO_SCOOTER)
-        main_page.find_element_with_wait(MainPageLocators.LOGO_SCOOTER)
-        assert driver.current_url == DataForTests.BASE_URL
+        main_page.wait_visibility_of_header_logo_scooter()
+        main_page.click_on_order_button_in_header()
+        main_page.wait_visibility_of_header_logo_scooter()
+        main_page.click_on_header_logo_scooter()
+        main_page.wait_visibility_of_header_logo_scooter()
+        assert main_page.current_url_is_base_url()
 
     @allure.title('Проверка,при клике на логотип Яндекса, в новом окне через редирект откроется главная страница Дзена')
     def test_logo_redirect_to_dzen(self, driver):
         main_page = MainPage(driver)
-        main_page.find_element_with_wait(MainPageLocators.LOGO_YANDEX)
-        main_page.click_to_element(MainPageLocators.LOGO_YANDEX)
+        main_page.wait_visibility_of_header_logo_yandex()
+        main_page.click_on_header_logo_yandex()
         main_page.switch_to_next_tab()
-        assert 'dzen' in driver.current_url
+        assert 'dzen' in main_page.get_current_url()
